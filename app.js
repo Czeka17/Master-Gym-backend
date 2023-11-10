5f4227f058f5276d598bd464457323b17c17f493
Jakub <jczekanski123@gmail.com>
2023-11-10 13:03:59 +0100

    small fixes in app.js

--- app.js
+++ app.js
@@ -41,9 +41,12 @@ app.use((error, req, res, next) => {
 	res.json({ message: error.message || "An unknown error occured!" });
 });
 mongoose
-  .connect(`${process.env.MONGODB}`)
+  .connect(`mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@dejv.p9bjtsg.mongodb.net/`)
   .then(() => {
-    app.listen(3000);
+	const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
   })
   .catch(err => {
     console.log(err)
